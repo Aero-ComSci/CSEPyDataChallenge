e52eac19cbb48f99f1b0dcbb43a5cac7c1b4804c
Mr. Baez <baezlopezolsen@dublinusd.org>
2025-03-31 21:16:35 -0700

    Added initial code for the challenge.

---
+++ challengeCode.py
@@ -0,0 +1,20 @@
+'''
+In this challenge you will be modifying the following function.
+1. Take in the arguement and find out what the type is
+2. Use the data in an useful way, if its a string concatenate
+    if its a number add to it. etc.
+3. Return the arguement with something new that you aggregated or modified,
+    just make sure you return the same data type that went in.'''
+
+def challenge(arg1):
+    #Do something with arg1 here
+    
+    return (arg1)
+
+#checks
+print(challenge("hi"))
+#Should print "hi" and something else, like "hi!"
+print(challenge(5))
+print(challenge(3.14))
+print(challenge(False))
+
